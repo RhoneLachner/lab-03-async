0fdef23bebd7b4a5d50c8284c13c98d7061bd73f
Rhone Lachner <fernandclay@gmail.com>
2020-12-02 15:08:13 -0800

    added files and copy finction works properly

---
+++ index.js
@@ -0,0 +1,31 @@
+const { mainModule } = require('process');
+
+const fs = require('fs').promises;
+
+
+// fs.readFile('./README.md', 'utf-8')
+// .then(data => {
+//     console.log(data);
+// })
+
+// fs.writeFile('./example.txt', 'hello i am a random string')
+// console.log('file written')
+
+
+// fs.readFile('./README.md', 'utf-8')
+// .then(data => {
+//     fs.writeFile('./READMEcopy2.txt', data)  
+// })
+// .then(() => console.log('DONE!'))
+
+
+const copy = (src, dst) => {
+    return fs.readFile(src, 'utf-8')
+        .then(data => {
+            fs.writeFile(dst, data)
+        })
+}
+
+// copy('./README.md', 'thirdcopytest.txt')
+
+mainModule.exports = copy;
